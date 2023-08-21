43c665f42e2ae8c396db35d6d9a1198d9c79cca6
PasqualeIsabelli <pasquale0395@gmail.com>
2023-08-21 16:52:21 +0200

    ex and bonus completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+}
+</script>
+
+
+<template>
+  <img class="size" src="../assets/1095_3x2.avif" alt="hero-image">
+</template>
+
+
+<style>
+.size {
+  width: 1000px;
+}
+</style>
